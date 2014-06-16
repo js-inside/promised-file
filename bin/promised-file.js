6624ef7d38268d8ef92d5427e951f692693956c7
wayneseymour <wayneseymour@gmail.com>
2014-06-16 12:28:21 -0400

    Wrapped in an iffy

--- bin/promised-file.js
+++ bin/promised-file.js
@@ -1,60 +1,62 @@
-/* jshint node:true */
-'use strict';
+(function () {
+  /* jshint node:true */
 
-var fs = require('fs');
-var glob = require('glob');
-var Promise = require('promise');
-var sprintf = require('sprintf');
+  'use strict';
 
-var globForFile;
-var readFile;
-var getFile;
+  var fs = require('fs');
+  var glob = require('glob');
+  var Promise = require('promise');
+  var sprintf = require('sprintf');
 
+  var globForFile;
+  var readFile;
+  var getFile;
 
-getFile = function getFile(filePath) {
-  return globForFile(filePath)
-    .then(readFile)
-    .then(function returnPromise(file) {
-      return file;
-    })
-    .catch (function catchErr(err) {
+  getFile = function getFile (filePath) {
+    return globForFile(filePath)
+      .then(readFile)
+      .then(function returnPromise (file) {
+        return file;
+      })
+      .catch(function catchErr (err) {
       var errMsg = sprintf('### Error: %s', err);
       //console.error(errMsg);
 
       return errMsg;
     });
-};
+  };
 
+  globForFile = function globForFile (file) {
+    return Promise.denodeify(glob)(file);
+  };
 
-globForFile = function globForFile(file) {
-  return Promise.denodeify(glob)(file);
-};
-
-readFile = function readFile(file) {
-  var promise = new Promise(function(resolve, reject) {
-    fs.readFile(file.path || file[0], 'utf8', function(err, res) {
-      if (err) reject(err);
-      if (! !! res) reject("File empty");
-      else resolve(res);
+  readFile = function readFile (file) {
+    var promise = new Promise(function (resolve, reject) {
+      fs.readFile(file.path || file[0], 'utf8', function (err, res) {
+        if (err) reject(err);
+        if (!!!res) reject("File empty");
+        else resolve(res);
+      });
     });
-  });
 
-  return promise;
-};
+    return promise;
+  };
 
-(function run() {
-  // If (require.main === module), this module was
-  // called directly like via the cli.
-  // If (require.main !== module), this module was
-  // "require'd" like var gll = require('./getLastLine');
-  if (require.main === module)
-    getFile( !! process.argv[2] ? process.argv[2] : '/Users/wkseymou/projects/chroma/chroma-manager/chroma_ui_new/static/chroma_ui/*.map')
-    .then(function(response) {
-      console.log('### response: \n\t%s', response);
-    })
-      .done();
+  (function run () {
+    // If (require.main === module), this module was
+    // called directly like via the cli.
+    // If (require.main !== module), this module was
+    // "require'd" like var gll = require('./getLastLine');
+    if (require.main === module)
+      getFile(!!process.argv[2] ? process.argv[2] : '/Users/wkseymou/projects/chroma/chroma-manager/chroma_ui_new/static/chroma_ui/*.map')
+        .then(function (response) {
+          console.log('### response: \n\t%s', response);
+        })
+        .done();
+  })();
+
+  module.exports = {
+    getFile: getFile
+  };
 })();
 
-module.exports = {
-  getFile: getFile
-};
