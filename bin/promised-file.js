4fa71eaa51d2b300f507adfe9aa8a6524caeeb9c
Wayne K Seymour III <wayneseymour@gmail.com>
2014-08-04 11:17:22 -0400

    A bit of cleanup based on a code review from a friend.
    I still think its not "there" yet.  But soon.  :)

--- bin/promised-file.js
+++ bin/promised-file.js
@@ -4,11 +4,30 @@
 var fs = require('fs');
 var Promise = require('promise');
 var globThen = Promise.denodeify(require('glob'));
-require('funky'); // Requiring truthy() and existy() in global scope.
+var f = require('funky');
 
 module.exports = {
   getFile: getFile
 };
+
+if (require.main === module) {
+
+  var fileArg = process.argv[2];
+
+  var getFileRunner = runIf(getFile.bind(null, fileArg), function handleErr (err) {
+    throw new Error('process.argv[2] is falsy: ', err);
+  });
+
+  getFileRunner(f.truthy(fileArg))
+    .then(function (response) {
+      console.log('### File [%s]: \n\t%s', fileArg, response);
+    })
+    .catch(function handleError(err) {
+      console.log('### Error: \n\t%s', err);
+    })
+    .done();
+
+}
 
 /**
  * Gets the file specified on process.argv[2].
@@ -16,10 +35,10 @@ module.exports = {
  * @param {String} filePath Path to the file.
  * @returns {Promise}
  */
-function getFile (filePath) {
+function getFile(filePath) {
   return globThen(filePath)
     .then(readFile)
-    .then(function returnPromise (file) {
+    .then(function returnPromise(file) {
       return file;
     });
 }
@@ -29,7 +48,7 @@ function getFile (filePath) {
  * @param {Object|Array} file
  * @returns {Promise}
  */
-function readFile (file) {
+function readFile(file) {
 
   return new Promise(function (resolve, reject) {
     fs.readFile(file.path || file[0], 'utf8', function (err, res) {
@@ -41,42 +60,19 @@ function readFile (file) {
 
 }
 
-(function run () {
-
-  // If (require.main === module), this module was
-  // called directly like via the cli.
-  // If (require.main !== module), this module was
-  // "require'd" like var gll = require('./getLastLine');
-  if (require.main === module) {
-
-    var fileArg = process.argv[2];
-
-    var getFileRunner = runIf(getFile.bind(null, fileArg));
-
-    getFileRunner(truthy(fileArg))
-      .then(function (response) {
-        console.log('### File [%s]: \n\t%s', fileArg, response);
-      })
-      .catch(function handleError (err) {
-        console.log('### Error: \n\t%s', err);
-      })
-      .done();
-
-  }
-
-})();
-
 /**
  * Higher order function (HOF) that takes a function, and runs the function if
  * the predicate is true.
  * @param {Function} f
+ * @param {Function} errHandler Handles errors.  Works for now, but this is not optimal.
+ * Would be better to only have one function coming in.
  * @returns {Function}
  */
-function runIf (f) {
-  return function runFuncIf (predicate) {
+function runIf(f, errHandler) {
+  return function runFuncIf(predicate) {
     if (predicate)
       return f();
     else
-      throw new Error('process.argv[2] is falsy.');
+      errHandler();
   }
 }
